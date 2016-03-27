75152eee9985b8e22e1926c3ee1f650e1779fb2e
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-27 22:05:35 +0530

    Ingredient model added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327154241) do
+ActiveRecord::Schema.define(version: 20160327163148) do
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "ingredients", ["recipe_id"], name: "index_ingredients_on_recipe_id"
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
