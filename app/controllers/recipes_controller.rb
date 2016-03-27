d1f4b9b0ad31fd07aaa8870ea608a4d1eb782f4d
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-27 18:58:35 +0530

    Recipe controller updated and views created

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,24 +1,50 @@
 class RecipesController < ApplicationController
 	
+	before_action :get_recipe, only: [:show,:edit,:update,:destroy]
+
 	def index
+		@recipes = Recipe.all.order(created_at: :desc)
 	end
 
 	def show
 	end
 
 	def new
+		@recipe = Recipe.new
+		if @recipe.save
+			redirect_to @recipe
+		else
+			render 'new'
+		end
 	end
 
 	def create
+		@recipe = Recipe.new(recipe_params)
 	end
 
 	def edit
 	end
 
 	def update
+		if @recipe.update(recipe_params)
+			redirect_to @recipe
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@recipe.destroy
+		redirect_to root_path
 	end
+
+	private
+		def recipe_params
+			params.require(:recipe).permit()
+		end
+
+		def get_recipe
+			@recipe = Recipe.find(params[:id])
+		end
 
 end
