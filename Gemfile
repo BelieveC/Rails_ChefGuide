f861b9049a9467d12b45a876cc707484fe2492be
Chaitanya Yadav <cayadav2009@gmail.com>
2016-03-27 21:15:58 +0530

    Paperclip and Image field in form added

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'haml'
 
+gem'paperclip'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -40,7 +40,7 @@ class RecipesController < ApplicationController
 
 	private
 		def recipe_params
-			params.require(:recipe).permit(:title,:description)
+			params.require(:recipe).permit(:title,:description,:image)
 		end
 
 		def get_recipe
